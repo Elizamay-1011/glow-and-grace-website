e917325d0c88fe0a712bccd1d40b0fe32e5f0527
Eliza May Calisa <elizacalisa@gmail.com>
2025-12-17 10:12:08 +0800

    Initial React project setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Shop';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Glow and Grace</h1>
-      <p>Welcome to our beauty product universe.</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Shop" element={<Products />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
+
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h2>Glow and Grace</h2>
+      <p>Discover radiant beauty, naturally.</p>
+    </div>
+  );
+}
+export default Home;
